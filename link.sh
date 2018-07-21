e3deb22ab6ff5502c8c956f6511f6a53b763723f
goolord <zacharyachurchill@gmail.com>
2018-07-21 14:51:05 -0400

    whole ass shit

--- link.sh
+++ link.sh
@@ -7,9 +7,6 @@ ln -sf $DIR/.config/* $HOME/.config/
 # make shell-scripts and link shell-scripts
 mkdir $HOME/Documents/shell-scripts/; ln -sf $DIR/shell-scripts/* $HOME/Documents/shell-scripts/
 
-# SpaceVim config
-mkdir $HOME/.SpaceVim.d/; ln -sf $DIR/.SpaceVim.d/* $HOME/.SpaceVim.d/
-
 ln -sf $DIR/.oh-my-zsh/custom/themes/* $HOME/.oh-my-zsh/custom/themes/
 ln -sf $DIR/.oh-my-zsh/custom/plugins/* $HOME/.oh-my-zsh/custom/plugins/
 
