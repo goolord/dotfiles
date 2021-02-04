fe92aaa477f5348808f4d670a0599d998ce32a1a
Zachary Churchill <zacharyachurchill@gmail.com>
2021-02-04 01:33:55 -0500

    icon

--- .config/nvim/lua/plugins/clap.lua
+++ .config/nvim/lua/plugins/clap.lua
@@ -1,2 +1,8 @@
 vim.g.clap_disable_run_rooter = true
 vim.g.clap_enable_icon = 1
+vim.g.clap_current_selection_sign = { 
+    text = '', 
+    texthl = 'ClapCurrentSelectionSign',
+    linehl = 'ClapCurrentSelection' 
+}
+
