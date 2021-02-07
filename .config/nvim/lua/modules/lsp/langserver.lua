8f74e0874924238c228b953cd4430a8583c09a22
Zachary Churchill <zacharyachurchill@gmail.com>
2021-02-07 03:40:44 -0500

    move lsputils stuff to custom_on_attach

--- .config/nvim/lua/modules/lsp/langserver.lua
+++ .config/nvim/lua/modules/lsp/langserver.lua
@@ -4,6 +4,15 @@ local lspconfig = require('lspconfig')
 -- in buffers where lsp is on
 local function custom_on_attach()
     require('modules.lsp.keybindings')
+    -- nvim-lsputils
+    vim.lsp.handlers['textDocument/codeAction']     = require'lsputil.codeAction'.code_action_handler
+    vim.lsp.handlers['textDocument/references']     = require'lsputil.locations'.references_handler
+    vim.lsp.handlers['textDocument/definition']     = require'lsputil.locations'.definition_handler
+    vim.lsp.handlers['textDocument/declaration']    = require'lsputil.locations'.declaration_handler
+    vim.lsp.handlers['textDocument/typeDefinition'] = require'lsputil.locations'.typeDefinition_handler
+    vim.lsp.handlers['textDocument/implementation'] = require'lsputil.locations'.implementation_handler
+    vim.lsp.handlers['textDocument/documentSymbol'] = require'lsputil.symbols'.document_handler
+    vim.lsp.handlers['workspace/symbol']            = require'lsputil.symbols'.workspace_handler
 end
 
 local defaultOpts = { on_attach = custom_on_attach }
