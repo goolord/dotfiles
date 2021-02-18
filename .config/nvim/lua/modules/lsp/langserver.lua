5621677a9dd7402ecf1ce430d6b30bb04b5e48f0
Zachary Churchill <zacharyachurchill@gmail.com>
2021-02-18 08:57:01 -0500

    lsp buffer local stuff, slightly better behaved blacklist

--- .config/nvim/lua/modules/lsp/langserver.lua
+++ .config/nvim/lua/modules/lsp/langserver.lua
@@ -1,24 +1,6 @@
 local lspconfig = require('lspconfig')
 
--- should figure out how to only remap these keybindings
--- in buffers where lsp is on
-local function custom_on_attach()
-    require('modules.lsp.keybindings')
-    -- nvim-lsputils
-    vim.lsp.handlers['textDocument/codeAction']     = require'lsputil.codeAction'.code_action_handler
-    vim.lsp.handlers['textDocument/references']     = require'lsputil.locations'.references_handler
-    vim.lsp.handlers['textDocument/definition']     = require'lsputil.locations'.definition_handler
-    vim.lsp.handlers['textDocument/declaration']    = require'lsputil.locations'.declaration_handler
-    vim.lsp.handlers['textDocument/typeDefinition'] = require'lsputil.locations'.typeDefinition_handler
-    vim.lsp.handlers['textDocument/implementation'] = require'lsputil.locations'.implementation_handler
-    vim.lsp.handlers['textDocument/documentSymbol'] = require'lsputil.symbols'.document_handler
-    vim.lsp.handlers['workspace/symbol']            = require'lsputil.symbols'.workspace_handler
-end
-
-local defaultOpts = { on_attach = custom_on_attach }
-
 -- a bit of a hack :/
-local cwd = vim.fn.getcwd()
 local blacklist = { "/home/zach/Dev/smurf" }
 
 local function in_blacklist(dir)
@@ -32,9 +14,41 @@ local function in_blacklist(dir)
     return res
 end
 
-if not in_blacklist(vim.fn.getcwd()) then
-    lspconfig.hls.setup(defaultOpts)
+-- should figure out how to only remap these keybindings
+-- in buffers where lsp is on
+local function custom_on_attach(client)
+    if in_blacklist(vim.fn.getcwd()) then
+        client.stop()
+        return
+    end
+
+    local function buf_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+    local function buf_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+    -- keymaps
+    buf_keymap('n', 'K'        , ':lua vim.lsp.buf.hover()<CR>'             , {noremap = false, silent = true})
+    buf_keymap('n', '<C-]>'    , ':lua vim.lsp.buf.definition()<CR>'        , {noremap = false, silent = true})
+    buf_keymap('n', 'gA'       , ':lua vim.lsp.buf.code_action()<CR>'       , {noremap = true , silent = true})
+    buf_keymap('n', 'gd'       , ':lua vim.lsp.buf.definition()<CR>'        , {noremap = true , silent = true})
+    buf_keymap('n', 'g['       , ':lua vim.lsp.diagnostic.goto_prev()<CR>'  , {noremap = true , silent = true})
+    buf_keymap('n', 'g]'       , ':lua vim.lsp.diagnostic.goto_next()<CR>'  , {noremap = true , silent = true})
+    buf_keymap('n', 'gl'       , ':lua vim.lsp.diagnostic.set_loclist()<CR>', {noremap = true , silent = true})
+    buf_keymap('n', 'gr'       , ':lua vim.lsp.buf.references()<CR>'        , {noremap = true , silent = true})
+    buf_keymap('n', 'gR'       , ':lua vim.lsp.buf.rename()<CR>'            , {noremap = true , silent = true})
+    buf_keymap('n', '<leader>F', ':lua vim.lsp.buf.formatting()<CR>'        , {noremap = true , silent = true})
+    buf_keymap('v', '<leader>F', ':lua vim.lsp.buf.formatting()<CR>'        , {noremap = true , silent = true})
+    -- nvim-lsputils
+    vim.lsp.handlers['textDocument/codeAction']     = require'lsputil.codeAction'.code_action_handler
+    vim.lsp.handlers['textDocument/references']     = require'lsputil.locations'.references_handler
+    vim.lsp.handlers['textDocument/definition']     = require'lsputil.locations'.definition_handler
+    vim.lsp.handlers['textDocument/declaration']    = require'lsputil.locations'.declaration_handler
+    vim.lsp.handlers['textDocument/typeDefinition'] = require'lsputil.locations'.typeDefinition_handler
+    vim.lsp.handlers['textDocument/implementation'] = require'lsputil.locations'.implementation_handler
+    vim.lsp.handlers['textDocument/documentSymbol'] = require'lsputil.symbols'.document_handler
+    vim.lsp.handlers['workspace/symbol']            = require'lsputil.symbols'.workspace_handler
 end
 
+local defaultOpts = { on_attach = custom_on_attach }
+
+lspconfig.hls.setup(defaultOpts)
 lspconfig.rls.setup(defaultOpts)
 -- lspconfig.elmls.setup{}
