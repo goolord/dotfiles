cbfa46857ce8867d9986057433c7c2431e9ca12b
Zachary Churchill <zacharyachurchill@gmail.com>
2021-02-23 10:56:30 -0500

    fix nvimTree... why this

--- .config/nvim/lua/plugins/nvimTree.lua
+++ .config/nvim/lua/plugins/nvimTree.lua
@@ -1,23 +1,25 @@
--- vim.g.nvim_tree_side = 'left'
--- vim.g.nvim_tree_width = 35
 vim.g.nvim_tree_ignore = {'.git', 'node_modules', '.cache'}
 vim.g.nvim_tree_follow = 1
 vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_hide_dotfiles = 1
-vim.g.nvim_tree_root_folder_modifier = ':~'
+
+local function get_lua_cb(cb_name)
+  return string.format(":lua require'nvim-tree'.on_keypress('%s')<CR>", cb_name)
+end 
 
 vim.g.nvim_tree_bindings = {
-    edit = {'l', 'o', '<cr>'},
-    toggle_ignored = 'I',
-    toggle_dotfiles = 'H',
-    refresh = 'R',
-    preview = '=',
+    ['l']    = get_lua_cb("edit"),
+    ['o']    = get_lua_cb("edit"),
+    ['<cr>'] = get_lua_cb("edit"),
+    ['I']    = get_lua_cb("toggle_ignored"),
+    ['H']    = get_lua_cb("toggle_dotfiles"),
+    ['R']    = get_lua_cb("refresh"),
+    ['=']    = get_lua_cb("preview"),
 }
 
 vim.g.nvim_tree_icons = {
     default = '',
     symlink = '',
-    -- git = {unstaged = "✗", staged = "✓", unmerged = "", renamed = "➜", untracked = "★"},
     folder = {default = " ", open = " "},
 }
 
