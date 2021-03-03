fd067e566ab95f5f9ee48f3bd47326595657d363
Zachary Churchill <zacharyachurchill@gmail.com>
2021-03-03 01:30:37 -0500

    bugfixes, remove dep on vista

--- .config/nvim/lua/plugins/clap.lua
+++ .config/nvim/lua/plugins/clap.lua
@@ -2,6 +2,7 @@ return function()
     vim.g.clap_layout = { relative = 'editor' }
     vim.g.clap_disable_run_rooter = true
     vim.g.clap_enable_icon = 1
+    vim.g.clap_provider_grep_enable_icon = 1
     vim.g.clap_current_selection_sign = { 
         text = '', 
         texthl = 'ClapCurrentSelectionSign',
