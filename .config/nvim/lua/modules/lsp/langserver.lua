626badeb09fdeae44a4404f34dad25e41d11b66a
Zachary Churchill <zacharyachurchill@gmail.com>
2021-03-03 10:39:46 -0500

    lsp keybinds

--- .config/nvim/lua/modules/lsp/langserver.lua
+++ .config/nvim/lua/modules/lsp/langserver.lua
@@ -27,17 +27,19 @@ return function()
         local function buf_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
         local function buf_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
         -- keymaps
-        buf_keymap('n', 'K'        , ':lua vim.lsp.buf.hover()<CR>'             , {noremap = false, silent = true})
-        buf_keymap('n', '<C-]>'    , ':lua vim.lsp.buf.definition()<CR>'        , {noremap = false, silent = true})
-        buf_keymap('n', 'gA'       , ':lua vim.lsp.buf.code_action()<CR>'       , {noremap = true , silent = true})
-        buf_keymap('n', 'gd'       , ':lua vim.lsp.buf.definition()<CR>'        , {noremap = true , silent = true})
-        buf_keymap('n', 'g['       , ':lua vim.lsp.diagnostic.goto_prev()<CR>'  , {noremap = true , silent = true})
-        buf_keymap('n', 'g]'       , ':lua vim.lsp.diagnostic.goto_next()<CR>'  , {noremap = true , silent = true})
-        buf_keymap('n', 'gl'       , ':lua vim.lsp.diagnostic.set_loclist()<CR>', {noremap = true , silent = true})
-        buf_keymap('n', 'gr'       , ':lua vim.lsp.buf.references()<CR>'        , {noremap = true , silent = true})
-        buf_keymap('n', 'gR'       , ':lua vim.lsp.buf.rename()<CR>'            , {noremap = true , silent = true})
-        buf_keymap('n', '<leader>F', ':lua vim.lsp.buf.formatting()<CR>'        , {noremap = true , silent = true})
-        buf_keymap('v', '<leader>F', ':lua vim.lsp.buf.formatting()<CR>'        , {noremap = true , silent = true})
+        buf_keymap('n', 'K'        , ':lua vim.lsp.buf.hover()<CR>'                       , {noremap = false, silent = true})
+        buf_keymap('n', '<C-]>'    , ':lua vim.lsp.buf.definition()<CR>'                  , {noremap = false, silent = true})
+        buf_keymap('n', 'gc'       , ':lua vim.lsp.buf.code_action()<CR>'                 , {noremap = true , silent = true})
+        buf_keymap('n', 'gd'       , ':lua vim.lsp.buf.definition()<CR>'                  , {noremap = true , silent = true})
+        buf_keymap('n', 'gf'       , ':lua vim.lsp.buf.workspace_symbol()<CR>'            , {noremap = true , silent = true})
+        buf_keymap('n', '<C-e>'    , ':lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', {noremap = true , silent = true})
+        buf_keymap('n', 'g['       , ':lua vim.lsp.diagnostic.goto_prev()<CR>'            , {noremap = true , silent = true})
+        buf_keymap('n', 'g]'       , ':lua vim.lsp.diagnostic.goto_next()<CR>'            , {noremap = true , silent = true})
+        buf_keymap('n', 'gl'       , ':lua vim.lsp.diagnostic.set_loclist()<CR>'          , {noremap = true , silent = true})
+        buf_keymap('n', 'gr'       , ':lua vim.lsp.buf.references()<CR>'                  , {noremap = true , silent = true})
+        buf_keymap('n', 'gR'       , ':lua vim.lsp.buf.rename()<CR>'                      , {noremap = true , silent = true})
+        buf_keymap('n', '<leader>F', ':lua vim.lsp.buf.formatting()<CR>'                  , {noremap = true , silent = true})
+        buf_keymap('v', '<leader>F', ':lua vim.lsp.buf.formatting()<CR>'                  , {noremap = true , silent = true})
         -- nvim-lsputils
         vim.lsp.handlers['textDocument/codeAction']     = require'lsputil.codeAction'.code_action_handler
         vim.lsp.handlers['textDocument/references']     = require'lsputil.locations'.references_handler
