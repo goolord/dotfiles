8c36a75b9cd17ad2c67e3e63f0e20b57cf2a8261
Zachary Churchill <zacharyachurchill@gmail.com>
2021-03-14 03:57:48 -0400

    lightbulb

--- home/.config/nvim/lua/modules/lsp/init.lua
+++ home/.config/nvim/lua/modules/lsp/init.lua
@@ -1,4 +1,6 @@
 return function()
     require('modules.lsp.langserver')()
     require('modules.lsp.diagnostic')()
+    vim.cmd [[autocmd CursorHold,CursorHoldI * lua require'nvim-lightbulb'.update_lightbulb()]]
+    vim.fn.sign_define('LightBulbSign', { text = "", texthl = "Type", linehl="", numhl="" })
 end
