19dc7d0e80cfaedacb721b7eb6f9bd5d2f3ba072
Zachary Churchill <zacharyachurchill@gmail.com>
2021-03-29 08:20:41 -0400

    toggleterm

--- home/.config/nvim/lua/plugins/toggleterm.lua
+++ home/.config/nvim/lua/plugins/toggleterm.lua
@@ -1,11 +1,12 @@
 return function()
-    require"toggleterm".setup{
+    require"toggleterm".setup {
       size = 80,
       open_mapping = [[<Leader>t]],
       shade_filetypes = {},
       shade_terminals = true,
       start_in_insert = true,
       persist_size = true,
+      insert_mappings = false,
       direction = 'vertical',
     }
 end
