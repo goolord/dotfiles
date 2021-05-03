c4b571432b64673cb479179c385b14000eaac2e2
Zachary Churchill <zacharyachurchill@gmail.com>
2021-05-03 10:36:58 -0400

    exts

--- home/.config/nvim/lua/plugins/statusline.lua
+++ home/.config/nvim/lua/plugins/statusline.lua
@@ -6,6 +6,7 @@ return function()
             section_separators = {'', ''},
             icons_enabled = true,
         },
+        extensions = { 'fugitive', 'nvim-tree' },
 
         sections = require'lualine.defaults'.sections,
         inactive_sections = require'lualine.defaults'.inactive_sections
