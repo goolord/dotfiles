62df060571a08bd710a629433dd0c82254a82e10
Zachary Churchill <zacharyachurchill@gmail.com>
2021-05-10 20:55:42 -0400

    discord

---
+++ home/.config/Lightcord_BD/themes/Tranquil.theme.css
@@ -0,0 +1,35 @@
+/**
+ * @name         Tranquil
+ * @description  Phoenix Theme Bundle: Primarily dark theme that's easy on the eyes [Also supports light theme which is less easy on the eyes]
+ * @author       MonsterDev
+ * @version      1.5
+ * @donate       https://goo.gl/9S8VGb
+ * @source       https://github.com/monstrousdev/themes/theme-files/Tranquil.theme.css
+ * @invite       TeRQEPb
+ * @authorId     402272736665272320
+ */
+
+/* IMPORT CSS FROM GITHUB */
+@import "https://monstrousdev.github.io/themes/phoenix-bundle/tranquil.css";
+/*
+---------------------------------------------------------------------------------------------------------------------------------------------
+IMPORTANT: CHANGE DISCORDS THEME TO DARK THEME IN APPEARANCE SETTINGS FOR THIS THEME TO WORK!
+
+Registered: Black Box Theme
+
+---------------------------------------------------------------------------------------------------------------------------------------------
+===== CUSTOMIZABLE COLORS =====
+*/
+:root { 
+ --roundness: 0px; /* sets the roundness for avatars */
+ --main-color: #8d2036; /* The main color for your theme */
+ --hover-color: #6f182a; /* the secondary color for your theme, mainly used as hover color */
+ --home-color: #eee; /* the color of your home icon */
+ --shadow: rgba(0,0,0,.4); /* the color of the shadows in your theme */
+ }
+
+/*
+---------------------------------------------------------------------------------------------------------------------------------------------
+TROUBLESHOOTING: IF YOU NEED ANY HELP GO TO https://discord.gg/TeRQEPb
+---------------------------------------------------------------------------------------------------------------------------------------------
+*/ 
