32c0d344a66b90bcf3020ce0cfe76ef861383ab5
Zachary Churchill <zacharyachurchill@gmail.com>
2021-05-18 15:40:34 -0400

    fzf stuff

--- home/.config/zim/zim.zsh
+++ home/.config/zim/zim.zsh
@@ -157,3 +157,12 @@ bindkey "^[[1;3C" forward-word
 bindkey "^[[1;3D" backward-word
 bindkey '\e[1;5D' backward-word
 bindkey '\e[1;5C' forward-word
+
+# disable sort when completing `git checkout`
+zstyle ':completion:*:git-checkout:*' sort false
+# set descriptions format to enable group support
+zstyle ':completion:*:descriptions' format '[%d]'
+# set list-colors to enable filename colorizing
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+# preview directory's content with exa when completing cd
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'exa -1 --color=always $realpath'
