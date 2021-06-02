a2152ec94a4a2cd6f005c46e164225d701c6dd50
Zachary Churchill <zacharyachurchill@gmail.com>
2021-06-02 09:43:08 -0400

    clap stuff

--- home/.config/nvim/lua/plugins/clap.lua
+++ home/.config/nvim/lua/plugins/clap.lua
@@ -1,4 +1,5 @@
 return function()
+    vim.g.clap_popup_border = 'nil'
     vim.g.clap_theme = 'gruvbox_dark'
     vim.g.clap_layout = { relative = 'editor' }
     vim.g.clap_disable_run_rooter = true
