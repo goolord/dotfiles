73325ee37f69ae600f9f5770e1caac708395b86e
Zachary Churchill <zacharyachurchill@gmail.com>
2021-08-28 16:34:57 -0400

    lua langserver

--- home/.config/nvim/lua/modules/lsp/langserver.lua
+++ home/.config/nvim/lua/modules/lsp/langserver.lua
@@ -47,4 +47,31 @@ return function()
         on_attach = custom_on_attach,
         capabilities = capabilities
     }
+
+    local sumneko_root_path = 'usr/share/lua-language-server'
+    local sumneko_binary = '/usr/bin/lua-language-server'
+
+    local runtime_path = vim.split(package.path, ';')
+    table.insert(runtime_path, "lua/?.lua")
+    table.insert(runtime_path, "lua/?/init.lua")
+
+    lspconfig.sumneko_lua.setup {
+        on_attach = custom_on_attach,
+        cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
+        settings = {
+            Lua = {
+                runtime = {
+                    version = 'LuaJIT',
+                    path = runtime_path,
+                },
+                diagnostics = {
+                    globals = {'vim'},
+                },
+                workspace = {
+                    library = vim.api.nvim_get_runtime_file("", true),
+                },
+                telemetry = { enable = false },
+            },
+        },
+    }
 end
