57b7623ac0ad510f800bf6eaa114a7fa44d28bb5
Zachary Churchill <zacharyachurchill@gmail.com>
2021-09-10 21:11:33 -0400

    gruvbox and >_> chromium since firefox is bugged

--- appearance.sh
+++ appearance.sh
@@ -1,4 +1,4 @@
 gsettings set org.gnome.desktop.interface cursor-theme "default"
 gsettings set org.gnome.desktop.interface icon-theme "Numix"
-gsettings set org.gnome.desktop.interface gtk-theme "oomox-classic_x_new"
+gsettings set org.gnome.desktop.interface gtk-theme "oomox-gruvbox-light-medium"
 gsettings set org.gnome.desktop.interface cursor-size 36
