8decd3f9551fd7eb5bd60e5f9b75774e78b1ede2
Zachary Churchill <zacharyachurchill@gmail.com>
2021-09-16 23:54:46 -0400

    disable default Tabular maps

--- home/.config/nvim/lua/plugins/unload.lua
+++ home/.config/nvim/lua/plugins/unload.lua
@@ -1,3 +1,4 @@
+-- built in
 vim.g.loaded_2html_plugin = 1
 vim.g.loaded_getscriptPlugin = 1
 vim.g.loaded_gzip = 1
@@ -14,5 +15,6 @@ vim.g.loaded_tutor_mode_plugin = 1
 vim.g.loaded_vimballPlugin = 1
 vim.g.loaded_zipPlugin = 1
 vim.g.loaded_zip = 1
+vim.g.no_default_tabular_maps = 1
 -- vim.g.loaded_matchit = 1
 -- vim.g.loaded_matchparen = 1
