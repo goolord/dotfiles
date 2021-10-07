86d977ca2791a593ccd21af6471aee49aeb8fdf3
Zachary Churchill <zacharyachurchill@gmail.com>
2021-10-07 16:16:27 -0400

    mickey mouse bs

--- home/.config/nvim-code/init.lua
+++ home/.config/nvim-code/init.lua
@@ -1,12 +1,4 @@
-vim.opt.rtp:remove('/home/zach/.config/nvim/after')
-vim.opt.rtp:remove('/home/zach/.config/nvim')
-vim.opt.rtp:remove('/home/zach/.local/share/nvim/site/after')
-vim.opt.rtp:remove('/home/zach/.local/share/nvim/site/pack/*/start/*')
-vim.opt.rtp:remove('/home/zach/.local/share/nvim/site/pack/*/start/*/after')
-vim.opt.rtp:remove('/home/zach/.local/share/nvim/site')
-
 vim.opt.rtp:append('/home/zach/.config/nvim-code')
 
 require('plugins.unload')
 require('settings')
-
