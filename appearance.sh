682657b8f3f1894876ded6b28cadcfe342db44a7
Zachary Churchill <zacharyachurchill@gmail.com>
2022-07-14 18:42:03 -0400

    foo

--- appearance.sh
+++ appearance.sh
@@ -2,3 +2,4 @@ gsettings set org.gnome.desktop.interface cursor-theme "default"
 gsettings set org.gnome.desktop.interface icon-theme "Numix"
 gsettings set org.gnome.desktop.interface gtk-theme "oomox-classic_x_new"
 gsettings set org.gnome.desktop.interface cursor-size 36
+sudo grub-mkconfig -o /boot/grub/grub.cfg
