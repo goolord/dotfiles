3d24b17fa90322fabd2c646d0979cb02786a5487
Zachary Churchill <zacharyachurchill@gmail.com>
2022-08-21 01:18:40 -0400

    tabular

--- home/.config/nvim-code/init.lua
+++ home/.config/nvim-code/init.lua
@@ -1,4 +1,5 @@
 vim.opt.rtp:append(os.getenv('HOME') .. '/.config/nvim-code')
+vim.opt.rtp:append(os.getenv('HOME') .. '/.local/share/nvim/site/pack/packer/start/tabular/')
 
 require('plugins.unload')
 require('settings')
--- home/.config/nvim-code/lua/keybindings.lua
+++ home/.config/nvim-code/lua/keybindings.lua
@@ -8,4 +8,21 @@ keymap('', '<Leader>ff', "<Cmd>call VSCodeNotify('workbench.action.quickOpen')<C
 keymap('', '<Leader>fg', "<Cmd>call VSCodeNotify('search.action.openEditor')<CR>", {silent = true, nowait = true, remap = true})
 keymap('', '<Leader>d', "<Cmd>call VSCodeNotify('workbench.action.toggleSidebarVisibility')<CR>", {silent = true, nowait = true, remap = true})
 keymap('', '<Leader>t', "<Cmd>call VSCodeNotify('workbench.action.terminal.toggleTerminal')<CR>", {silent = true, nowait = true, remap = true})
-keymap('', '<Esc>', "<Cmd>noh<CR>", {silent = true, nowait = true, remap = false})
+keymap('n', '<Esc>', "<Cmd>noh<CR>", {silent = true, nowait = true, remap = false})
+
+local function tabularize(key, regex)
+    keymap('', '<Leader>a' .. key, ':Tabularize ' .. regex, {silent = true, nowait = false, remap = false})
+end
+
+tabularize('a', '/')
+tabularize('(', '/(/r0<CR>')
+tabularize(')', '/)/l0<CR>')
+tabularize('[', '/[/r0<CR>')
+tabularize(']', '/]/l0<CR>')
+tabularize('{', '/{<CR>')
+tabularize('}', '/}<CR>')
+tabularize(':', '/:\\+<CR>')
+tabularize('<', '/<\\S*><CR>')
+tabularize('>', '/\\S*><CR>')
+tabularize('=', '/=\\S*<CR>')
+tabularize(',', '/,/l0r1<CR>')
