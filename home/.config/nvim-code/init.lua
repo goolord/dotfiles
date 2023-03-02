2f96b70ba7925c03873283359f999fd0f7e7a3e2
Zachary Churchill <zacharyachurchill@gmail.com>
2023-03-02 14:17:21 -0500

    lazy

--- home/.config/nvim-code/init.lua
+++ home/.config/nvim-code/init.lua
@@ -1,5 +1,5 @@
 vim.opt.rtp:append(os.getenv('HOME') .. '/.config/nvim-code')
-vim.opt.rtp:append(os.getenv('HOME') .. '/.local/share/nvim/site/pack/packer/start/tabular/')
+vim.opt.rtp:append(os.getenv('HOME') .. '.local/share/nvim/lazy/tabular/')
 
 require('plugins.unload')
 require('settings')
