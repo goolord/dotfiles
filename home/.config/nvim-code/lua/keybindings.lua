850cfee2ebca9fe4c32452e79152d49a30ec7d42
Zachary Churchill <zacharyachurchill@gmail.com>
2023-03-02 15:32:18 -0500

    nvim-code

--- home/.config/nvim-code/lua/keybindings.lua
+++ home/.config/nvim-code/lua/keybindings.lua
@@ -1,17 +1,22 @@
 local keymap = vim.keymap.set
 
-keymap('', '<Space>', '<Leader>', {silent = true, remap = true} )
+keymap('', '<Space>', '<Leader>', { silent = true, remap = true })
 -- disable keys
-keymap('' , '<MiddleMouse>', '<Nop>', {silent = true, nowait = true, remap = true } )
-keymap('i', '<MiddleMouse>', '<Nop>', {silent = true, nowait = true, remap = true} )
-keymap('', '<Leader>ff', "<Cmd>call VSCodeNotify('workbench.action.quickOpen')<CR>", {silent = true, nowait = true, remap = true})
-keymap('', '<Leader>fg', "<Cmd>call VSCodeNotify('search.action.openEditor')<CR>", {silent = true, nowait = true, remap = true})
-keymap('', '<Leader>d', "<Cmd>call VSCodeNotify('workbench.action.toggleSidebarVisibility')<CR>", {silent = true, nowait = true, remap = true})
-keymap('', '<Leader>t', "<Cmd>call VSCodeNotify('workbench.action.terminal.toggleTerminal')<CR>", {silent = true, nowait = true, remap = true})
-keymap('n', '<Esc>', "<Cmd>noh<CR>", {silent = true, nowait = true, remap = false})
+keymap('', '<MiddleMouse>', '<Nop>', { silent = true, nowait = true, remap = true })
+keymap('i', '<MiddleMouse>', '<Nop>', { silent = true, nowait = true, remap = true })
+keymap('', '<Leader>ff', "<Cmd>call VSCodeNotify('workbench.action.quickOpen')<CR>",
+    { silent = true, nowait = true, remap = true })
+keymap('', '<Leader>fg', "<Cmd>call VSCodeNotify('search.action.openEditor')<CR>",
+    { silent = true, nowait = true, remap = true })
+keymap('', '<Leader>d', function()
+    vim.fn.VSCodeNotify('workbench.action.toggleSidebarVisibility')
+end, { silent = true, nowait = true, remap = true })
+keymap('', '<Leader>t', "<Cmd>call VSCodeNotify('workbench.action.terminal.toggleTerminal')<CR>",
+    { silent = true, nowait = true, remap = true })
+keymap('n', '<Esc>', "<Cmd>noh<CR>", { silent = true, nowait = true, remap = false })
 
 local function tabularize(key, regex)
-    keymap('', '<Leader>a' .. key, ':Tabularize ' .. regex, {silent = true, nowait = false, remap = false})
+    keymap('', '<Leader>a' .. key, ':Tabularize ' .. regex, { silent = true, nowait = false, remap = false })
 end
 
 tabularize('a', '/')
@@ -26,3 +31,8 @@ tabularize('<', '/<\\S*><CR>')
 tabularize('>', '/\\S*><CR>')
 tabularize('=', '/=\\S*<CR>')
 tabularize(',', '/,/l0r1<CR>')
+
+keymap('', '<C-h>', '<cmd>wincmd h<cr>', 'Window left', { silent = true, nowait = true, remap = true })
+keymap('', '<C-j>', '<cmd>wincmd j<cr>', 'Widnow down', { silent = true, nowait = true, remap = true })
+keymap('', '<C-k>', '<cmd>wincmd k<cr>', 'Window up', { silent = true, nowait = true, remap = true })
+keymap('', '<C-l>', '<cmd>wincmd l<cr>', 'Window right', { silent = true, nowait = true, remap = true })
