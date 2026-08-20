49c1f4d8db23701bbdc54ffafc31ecdc057393f7
Zachary Churchill <zacharyachurchill@gmail.com>
2026-08-20 12:42:23 -0400

    Switch to compact iosevka.tar.xz (10MB) with core unified font styles

--- fonts/mkfont.sh
+++ fonts/mkfont.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env zsh
 
-[ ! -d "./output/iosevka" ] && mkdir ./output/iosevka && tar -xzf ./iosevka.tar.gz -C ./output/iosevka
+[ ! -d "./output/iosevka" ] && mkdir -p ./output/iosevka && tar -xf ./iosevka.tar.xz -C ./output/iosevka
 if [ ! -d "./ouptut/font-patcher" ]; then
   curl "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FontPatcher.zip" -L --output "output/font-patcher.zip"
   unzip -o output/font-patcher.zip -d output/font-patcher
